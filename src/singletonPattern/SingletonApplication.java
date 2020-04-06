09107218bf046a156dcca6a6410a7dc3d5adae1a
Marffm1109 <marceloffmartins@hotmail.com>
2020-04-06 08:06:31 +0200

    Singleton Implementation

--- src/singletonPattern/SingletonApplication.java
+++ src/singletonPattern/SingletonApplication.java
@@ -3,7 +3,13 @@ package singletonPattern;
 public class SingletonApplication {
 
   public static void main(String[] args) {
-    System.out.println("This is a test");
+    SingletonClassInstance singletonClassInstance1 = SingletonClassInstance.getInstance();
+
+    System.out.println(singletonClassInstance1);
+
+    SingletonClassInstance singletonClassInstance2 = SingletonClassInstance.getInstance();
+
+    System.out.println(singletonClassInstance2);
   }
 
 }
---
+++ src/singletonPattern/SingletonClassInstance.java
@@ -0,0 +1,18 @@
+package singletonPattern;
+
+public class SingletonClassInstance {
+
+  private static SingletonClassInstance singletonClassInstance;
+
+  private SingletonClassInstance(){}
+
+  public static SingletonClassInstance getInstance(){
+    if (singletonClassInstance == null)
+      synchronized (SingletonClassInstance.class) {
+        if (singletonClassInstance == null)
+          singletonClassInstance = new SingletonClassInstance();
+      }
+
+    return singletonClassInstance;
+  }
+}
